2fbbf162f6140b535d9697b9cc8128c5990e1c6e
Andrzej Palec <andrzej@Andrzejs-MacBook-Pro.local>
2017-06-11 15:21:08 +0200

    Lecture 139 COMPLETED

--- Pokemon/CoreDataHelp.swift
+++ Pokemon/CoreDataHelp.swift
@@ -46,13 +46,13 @@ func getAllPokemon() -> [Pokemon] {
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     do {
-    let pokemons = try context.fetch(Pokemon.fetchRequest()) as! [Pokemon]
+        let pokemons = try context.fetch(Pokemon.fetchRequest()) as! [Pokemon]
         
         if pokemons.count == 0 {
             addAllPokemon()
             return getAllPokemon()
         }
-    return pokemons
+        return pokemons
     } catch {}
     
     return []
@@ -60,10 +60,34 @@ func getAllPokemon() -> [Pokemon] {
 }
 
 func getAllCaughtPokemons() -> [Pokemon] {
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
+    let fetchRequest = Pokemon.fetchRequest() as NSFetchRequest<Pokemon>
+    
+    fetchRequest.predicate = NSPredicate(format: "caught == %@", true as CVarArg)
+    
+    do {
+        
+        let pokemons = try context.fetch(fetchRequest) as [Pokemon]
+        
+        return pokemons
+    } catch{}
+    return []
 }
 
-func getAllUncaughtPokemons() -> [Pokemon]{
+func getAllUncaughtPokemons() -> [Pokemon] {
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
+    let fetchRequest = Pokemon.fetchRequest() as NSFetchRequest<Pokemon>
+    
+    fetchRequest.predicate = NSPredicate(format: "caught == %@", false as CVarArg)
+    
+    do {
+        
+        let pokemons = try context.fetch(fetchRequest) as [Pokemon]
+        
+        return pokemons
+    } catch{}
+    return []
 }
 
